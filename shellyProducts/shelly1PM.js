8c642ec7604ddbce3945b11799679f79f171a313
leonreucher <leonreucher@yahoo.de>
2023-08-31 00:03:14 +0200

    Removed plus/pro devices

--- shellyProducts/shelly1PM.js
+++
@@ -1,6 +0,0 @@
-import got from 'got'
-import { Shelly1 } from './shelly1.js';
-
-class Shelly1PM extends Shelly1 {
-    
-}
--- shellyProducts/shellyPro1PM.js
+++
@@ -1,9 +0,0 @@
-import got from 'got'
-import { ShellyPlus1PM } from './ShellyPlus1PM.js';
-
-
-class ShellyPro1PM extends ShellyPlus1PM {
-    
-}
-
-export { ShellyPro1PM };
